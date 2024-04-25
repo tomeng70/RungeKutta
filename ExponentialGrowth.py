4e23dce3bddefffeb7716d13da3936cd87da3da9
tomeng70 <tomeng70@users.noreply.github.com>
2024-04-25 07:15:48 -0400

    added RK method to estimate exponential growth.  note the RK method has higher error (magnitude than the basic method.

--- ExponentialGrowth.py
+++ ExponentialGrowth.py
@@ -11,9 +11,28 @@ def popExp(t):
     val = N0 * math.exp(R * t)
     return val
 
+def rateEqn(N, t):
+    # note for exponential growth growth, the rate does not depend on time.
+    # instead, it's based on the current population only.
+    # we will still pass time as an argument to this function, 
+    # even though it's not needed.
+    val = R * N
+    return val
+
 def calcNext(Nprev):
     val = Nprev / (1 - R * H)
     return val
+
+def calcRKNext(N, t):
+    # calculate the runge kutta terms.
+    k1 = rateEqn(N, t)
+    k2 = rateEqn(N + k1 / 2, t + H/2)
+    k3 = rateEqn(N + k2 / 2, t + H/2)
+    k4 = rateEqn(N + k3, t + H)
+    
+    # estimate the next value based on previous value.
+    nextVal = N + H * (k1 + 2 * k2 + 2 * k3 + k4) / 6
+    return nextVal
 
 def main():
     print("hello")
@@ -23,6 +42,7 @@ def main():
     tList = [0]
     NList = [N0]
     NEstList = [N0]
+    NRKList = [N0]
 
     while (t <= TIME_LIMIT):
         # use previous estimated pop
@@ -31,6 +51,10 @@ def main():
         NPrev = NEstList[-1]
         NNext = calcNext(NPrev)
         NEstList.append(NNext)
+
+        # use runge kutta method to get next
+        Nrk = calcRKNext(NPrev, t)
+        NRKList.append(Nrk)
 
         # add time to list
         t = t + deltaT
@@ -41,7 +65,7 @@ def main():
         NList.append(pop)
 
     # plot results. 
-    plt.plot( tList, NList, ".", tList, NEstList, '+')
+    plt.plot( tList, NList, ".", tList, NEstList, '+', tList, NRKList, "^")
     plt.title("Population Growth vs Time (sec)")
     plt.show()
 
